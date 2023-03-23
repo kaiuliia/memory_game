4f5735dc6d8cb09aea5c9799c62970cf337e9286
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-03-23 20:00:18 +0100

    imported useDispatch to the card

--- src/features/board/cardRow/card/card.js
+++ src/features/board/cardRow/card/card.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { selectVisibleIds } from '../../boardSlice'
+import { useSelector, useDispatch } from "react-redux";
+import { selectVisibleIds, flipCard } from '../../boardSlice';
+
 
 let cardLogo =
   "https://static-assets.codecademy.com/Courses/Learn-Redux/matching-game/codecademy_logo.png";
